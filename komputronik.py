69de3e3e4bd9db20621d67793f4515815d3ce9d5
Vasyl Karakevych <ny.vasyl@gmail.com>
2023-11-01 14:28:59 +0200

    1.1 add filter and sort

--- komputronik.py
+++ komputronik.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 import openpyxl
 from product import Product
 
-def open_surl(url):
+def Open_url(url):
     # Відправка запиту до сторінки і отримання її вмісту
     response = requests.get(url)
 
@@ -15,9 +15,7 @@ def open_surl(url):
         # Знаходимо всі елементи з назвою товару і ціною
         product_names = soup.find_all('h2', class_='font-headline text-lg font-bold leading-6 line-clamp-3 md:text-xl md:leading-8')
         prices = soup.find_all('div', class_='text-3xl font-bold leading-8')
-
-        workbook = openpyxl.Workbook()
-        sheet = workbook.active
+        # font-semibold
 
         # Виводимо дані на екран
         for row, (name, price) in enumerate(zip(product_names, prices), start=1):
@@ -26,22 +24,19 @@ def open_surl(url):
 
             product_price = ''.join(product_price[0:-3].split())
 
-            sheet.cell(row=row, column=1, value=product_name)
-            sheet.cell(row=row, column=2, value=product_price)
-
-         # Видаляємо якщо пише у назві OUTLET   
+        # Видаляємо якщо пише у назві OUTLET   
             if product_name[-7:-1] == "Outlet": 
                 continue
 
-            products = Product(product_name, product_price)
+            products = Product(product_name, int(product_price))
             list_products.append(products)
 
-        workbook.save('komputronik.xlsx')
+        
     else:
         print(f'Помилка отримання сторінки. Статус-код: {response.status_code}')
 # URL сторінки, яку потрібно спарсити
 
-def fiter_list(list_products):
+def Fiter_List(list_products):
     # Створюємо словник для відстеження унікальних імен
     unique_names = {}
 
@@ -53,22 +48,40 @@ def fiter_list(list_products):
             unique_names[product.name] = True
             filtered_products.append(product)
     list_products = filtered_products
-
-    # Фільтрумемо ті які містять в назві outlet
-    # for product in list_products:
-    #     if product.name.find('oferta Outlet'):
-    #         print('outlet')
-
     return list_products
-
+def Write_Exel(list_product):
+    workbook = openpyxl.Workbook()
+    sheet = workbook.active
+    
+    for row, (product) in enumerate(list_products, start=1):
+        sheet.cell(row=row, column=1, value=product.getName())
+        sheet.cell(row=row, column=2, value=product.getPrice())
+    
+    workbook.save('komputronik.xlsx')
+def Sort(list_products):
+    for i in range(0, len(list_products)-1):
+        for j in range(i, len(list_products)):
+            if list_products[i].getPrice() >  list_products[j].getPrice():
+                tmp_name = list_products[i].getName()
+                tmp_price = list_products[i].getPrice()
+                list_products[i].setName(list_products[j].getName())
+                list_products[i].setPrice(list_products[j].getPrice())
+                list_products[j].setName(tmp_name)
+                list_products[j].setPrice(tmp_price)
+                if i > 1:
+                    i = i - 2
+    return list_products
 
 list_products = []
 for i in range(1,4):
     url = f'https://www.komputronik.pl/search-filter/1099/geforce-rtx-3060?a%5B507%5D%5B%5D=130691&filter=1&showBuyActiveOnly=0&p={i}'
-    open_surl(url)
+    Open_url(url)
     print('*' * i)
 
-list_products = fiter_list(list_products)
+list_products = Fiter_List(list_products)
+Write_Exel(list_products)
 
 print(list_products)
-print(f'count= {len(list_products)}')
+Sort(list_products)
+print(list_products)
+# print(f'count= {len(list_products)}')
--- product.py
+++ product.py
@@ -6,4 +6,13 @@ class Product:
     def __repr__(self) -> str:
         return(f'NAME= {self.name}******* PRICE= {self.price}\n')
 
+    def getName(self) -> str:
+        return self.name
+    def getPrice(self) -> int:
+        return self.price
+    
+    def setName(self, name):
+        self.name = name
+    def setPrice(self, price):
+        self.price = price
 
